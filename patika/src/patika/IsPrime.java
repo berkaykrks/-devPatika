a733cdb77c18e6b3e09523fabe1409d68890727d
berkaykrks <ata.karakus@std.medipol.edu.tr>
2024-05-01 11:59:17 +0300

    Prime Sayi

---
+++ patika/src/patika/IsPrime.java
@@ -0,0 +1,25 @@
+package patika;
+
+public class IsPrime {
+	public static void main(String[] args) {
+        System.out.println("1 ile 100 arasındaki asal sayılar:");
+
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

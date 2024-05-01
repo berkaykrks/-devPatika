3ec767b659a6f60bbccb34d59bf6d11c4d714371
berkaykrks <ata.karakus@std.medipol.edu.tr>
2024-05-01 11:56:54 +0300

    TersUcgen

---
+++ patika/src/patika/TersUcgen.java
@@ -0,0 +1,22 @@
+package patika;
+
+import java.util.Scanner;
+
+public class TersUcgen {
+	  public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.print("Basamak Sayısı: ");
+	        int basamakSayisi = input.nextInt();
+
+	        for (int i = basamakSayisi; i >= 1; i--) {
+	            for (int j = 1; j <= basamakSayisi - i; j++) {
+	                System.out.print(" ");
+	            }
+	            for (int k = 1; k <= 2 * i - 1; k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    }
+}

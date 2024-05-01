c4498039e4a0f37cc3a4058c79abfc874a58c5a4
berkaykrks <ata.karakus@std.medipol.edu.tr>
2024-05-01 12:38:16 +0300

    Fibonacci Serisi Bulma

--- patika/src/patika/Fibonacci.java
+++ patika/src/patika/Fibonacci.java
@@ -1,5 +1,23 @@
 package patika;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fibonacci serisinin eleman sayısını giriniz: ");
+        int n = input.nextInt();
+
+        System.out.println(n + " Elemanlı Fibonacci Serisi:");
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
 }
